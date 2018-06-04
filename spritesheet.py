c4e5d88b6ed719490b2401a3442a33f547303be3
chronarchitect <chronarchitect@teletran>
2018-06-04 11:30:51 +0530

    Modified spritesheet.py to be more readable

--- spritesheet.py
+++ spritesheet.py
@@ -1,18 +1,17 @@
 import pygame
-pygame.init()
 
-class spritesheet(object):
+import constants
+
+class SpriteSheet(object):
 
     def __init__(self, filename):
         """
         Constructor
         """
-        #Call the parent class constructor
-        super().__init__()
-
         #Fetch the image and set it as sprite surface
         self.sheet = pygame.image.load(filename).convert()
-        
+    
+#Loads one image
     def image_at(self, rectangle, colorkey = None):
         "Loads image from x,y,x+offset,y+offset"
         rect = pygame.Rect(rectangle)
@@ -23,11 +22,13 @@ class spritesheet(object):
                 colorkey = image.getat((0,0))
             image.set_colorkey(colorkey, pygame,RLEACCEL)
         return image
-    #Load a whole bunch of images and return them as a list
+
+#Load a whole bunch of images and return them as a list
     def images_at(self, rects, colorkey = None):
         "Loads multiple images, supply a list of coordinates"
         return [self.image_at(rect, colorkey) for rect in rects]
-    #Load a whole strip of images
+    
+#Load a whole strip of images
     def load_strip(self, rect, image_count, colorkey = None):
         "Loads a strip of images and returns them as a list"
         tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3]) for x in range(image_count)]
